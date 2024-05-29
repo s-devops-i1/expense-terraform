c5ee0559a67cb65981caa08e95aa247f306fc27f
shuja <shuja.hafez@gmail.com>
2024-05-29 21:46:25 +0530

    pip3.11 fixed

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -19,7 +19,7 @@ resource "null_resource" "ansible" {
     }
 
     inline = [
-      "sudo pip3.22 install ansible",
+      "sudo pip3.11 install ansible",
       "ansible-pull -i localhost, -U https://github.com/s-devops-i1/expense-terraform.git -e env=${var.env} -e role_name=${var.component} expense-play.yml"
     ]
   }
