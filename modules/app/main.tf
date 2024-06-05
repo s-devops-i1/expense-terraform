b046206fb8edaec865b3406c38a20b00e4cd19ac
shuja <shuja.hafez@gmail.com>
2024-06-05 18:21:40 +0530

    spot instance creating

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -2,6 +2,16 @@ resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.image_id
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.selected.id]
+
+  instance_market_options {
+    market_type = "spot"
+    spot_options {
+
+      instance_interruption_behavior  =  "stop"
+      spot_instance_type              =  "persistent"
+    }
+  }
+
   tags = {
     Name    = var.component
     monitor = "yes"
