cecbcd55f147d6b9ed186a22de5b6a6a6305295a
shuja <shuja.hafez@gmail.com>
2024-06-22 19:31:22 +0530

    frontend and backend also updated

--- main.tf
+++ main.tf
@@ -1,24 +1,24 @@
-# module "frontend" {
-#   depends_on = [ module.backend]
-#
-#   source = "./modules/app"
-#   instance_type = var.instance_type
-#   component  = "frontend"
-#   zone_id    = var.zone_id
-#   env        = var.env
-#   vault_token = var.vault_token
-# }
-#
-# module "backend" {
-#   depends_on = [ module.mysql]
-#
-#   source = "./modules/app"
-#   instance_type = var.instance_type
-#   component  = "backend"
-#   zone_id    = var.zone_id
-#   env        = var.env
-#   vault_token = var.vault_token
-# }
+module "frontend" {
+  depends_on = [ module.backend]
+
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component  = "frontend"
+  zone_id    = var.zone_id
+  env        = var.env
+  vault_token = var.vault_token
+}
+
+module "backend" {
+  depends_on = [ module.mysql]
+
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component  = "backend"
+  zone_id    = var.zone_id
+  env        = var.env
+  vault_token = var.vault_token
+}
 
 module "mysql" {
   source = "./modules/app"
