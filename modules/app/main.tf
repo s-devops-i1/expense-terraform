00d91c587881005d116841433db809d71f4b4ce4
shuja <shuja.hafez@gmail.com>
2024-06-22 15:58:34 +0530

    public

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -32,7 +32,7 @@ resource "null_resource" "ansible" {
       type     = "ssh"
       user     = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_user
       password = jsondecode(data.vault_generic_secret.ssh.data_json).ansible_password
-      host     = aws_instance.instance.private_ip
+      host     = aws_instance.instance.public_ip
     }
 
     inline = [
