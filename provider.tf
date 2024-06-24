c45121ccb920405ea0149661fad76a7700e0e3a4
shuja <shuja.hafez@gmail.com>
2024-06-24 16:42:50 +0530

    all variables added

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-provider "vault" {
-  address = "https://vault.shujathdevops.online:8200"
-  token = var.vault_token
-  skip_tls_verify = true
-}
+# provider "vault" {
+#   address = "https://vault.shujathdevops.online:8200"
+#   token = var.vault_token
+#   skip_tls_verify = true
+# }
