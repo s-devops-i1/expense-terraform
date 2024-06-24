5c737f9469aa6e781fc5bb31a612c3ca1a5bb100
shuja <shuja.hafez@gmail.com>
2024-06-24 10:28:14 +0530

    peering connection creating

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,3 +16,14 @@ resource "aws_subnet" "zone-1" {
   }
 }
 
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = "vpc-0d777bc0eeb02d730"
+  vpc_id        = aws_vpc.dev.id
+  auto_accept   = true
+
+  tags  = {
+    Name = "peering"
+  }
+}
+
+
