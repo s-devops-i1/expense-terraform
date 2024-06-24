a2a7724a85f185656a2780a08a0e8445cfc7e170
shuja <shuja.hafez@gmail.com>
2024-06-24 08:41:08 +0530

    subnet creating

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "dev" {
     Name = var.env
   }
 }
+
+resource "aws_subnet" "zone-1" {
+  vpc_id     = aws_vpc.dev.id
+  cidr_block = "10.10.0.0/24"
+
+  tags = {
+    Name = "zone-1"
+  }
+}
+
