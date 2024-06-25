d8b9385c8ee4550c0713189d6a81c993c3364492
shuja <shuja.hafez@gmail.com>
2024-06-25 14:03:33 +0530

    provisioner commented

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -3,7 +3,7 @@ data "aws_ami" "ami" {
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
  }
-
-data "vault_generic_secret" "ssh" {
-  path = "common/common"
-}
+#
+# data "vault_generic_secret" "ssh" {
+#   path = "common/common"
+# }
