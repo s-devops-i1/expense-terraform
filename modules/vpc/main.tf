b73b1217a7a5ab0781518a032f3a3ecb2a1cb10f
shuja <shuja.hafez@gmail.com>
2024-06-25 20:26:15 +0530

    peer connection id updated

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -62,11 +62,11 @@ resource "aws_vpc_peering_connection" "peer" {
   }
 }
 
-# resource "aws_route" "main" {
-#   route_table_id            = aws_vpc.dev.default_route_table_id
-#   destination_cidr_block    = var.default_cidr_block
-#    vpc_peering_connection_id = var.vpc_peering_connection_id
-# }
+resource "aws_route" "main" {
+  route_table_id            = aws_vpc.dev.default_route_table_id
+  destination_cidr_block    = var.default_cidr_block
+   vpc_peering_connection_id = var.vpc_peering_connection_id
+}
 
 resource "aws_route" "default-vpc" {
   route_table_id            = var.default_route_table_id
