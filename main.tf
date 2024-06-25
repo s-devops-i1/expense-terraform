509daae6410dc5c2af4cd4a7cd86273ff6d3301e
shuja <shuja.hafez@gmail.com>
2024-06-25 12:58:20 +0530

    app module enabled

--- main.tf
+++ main.tf
@@ -1,33 +1,39 @@
-# module "frontend" {
-#   depends_on = [ module.backend]
-#
-#   source = "./modules/app"
-#   instance_type = var.instance_type
-#   component  = "frontend"
-#   zone_id    = var.zone_id
-#   env        = var.env
-#   vault_token = var.vault_token
-# }
-#
-# module "backend" {
-#   depends_on = [ module.mysql]
-#
-#   source = "./modules/app"
-#   instance_type = var.instance_type
-#   component  = "backend"
-#   zone_id    = var.zone_id
-#   env        = var.env
-#   vault_token = var.vault_token
-# }
-#
-# module "mysql" {
-#   source = "./modules/app"
-#   instance_type = var.instance_type
-#   component  = "mysql"
-#   zone_id    = var.zone_id
-#   env        = var.env
-#   vault_token = var.vault_token
-# }
+module "frontend" {
+  depends_on = [ module.backend]
+
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component  = "frontend"
+  zone_id    = var.zone_id
+  env        = var.env
+  vault_token = var.vault_token
+  subnets     = module.vpc.db_subnet
+  vpc_id      = module.vpc.vpc_id
+}
+
+module "backend" {
+  depends_on = [ module.mysql]
+
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component  = "backend"
+  zone_id    = var.zone_id
+  env        = var.env
+  vault_token = var.vault_token
+  subnets     = module.vpc.db_subnet
+  vpc_id      = module.vpc.vpc_id
+}
+
+module "mysql" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component   = "mysql"
+  zone_id     = var.zone_id
+  env         = var.env
+  vault_token = var.vault_token
+  subnets     = module.vpc.db_subnet
+  vpc_id      = module.vpc.vpc_id
+}
 
 
 module "vpc" {
