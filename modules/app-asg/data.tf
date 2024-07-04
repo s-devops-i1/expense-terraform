3e92bd533c2dd9fe29003d5fd87512d7c2a1a3f5
shuja <shuja.hafez@gmail.com>
2024-07-04 15:33:47 +0530

    changed the ami to golden ami

--- modules/app-asg/data.tf
+++ modules/app-asg/data.tf
@@ -1,7 +1,8 @@
 data "aws_ami" "ami" {
-  most_recent      = true
-  name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = ["973714476881"]
+  most_recent     = true
+  name_regex      = "golden-ami-*"
+#   owners        = ["973714476881"]
+  owners          = ["self"]
 }
 #
 data "vault_generic_secret" "ssh" {
